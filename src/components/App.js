9045451d4d8624c5be40599d52117a8df34e2c96
sl24 <slvitalik24@gmail.com>
2020-12-02 20:36:22 +0200

    Create App

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
